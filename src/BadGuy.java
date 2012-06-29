bbec6b28f3e62067d4d485c9dbe0c1326410bfb4
Jon Roster <jroster76@gmail.com>
2012-06-28 19:16:08 -0700

    -Methods stubbed out

---
+++ src/BadGuy.java
@@ -0,0 +1,13 @@
+
+public class BadGuy {
+
+	public BadGuy(EyeOfSauron eye, String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void defeated() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/EyeOfSauron.java
@@ -0,0 +1,9 @@
+
+public class EyeOfSauron {
+
+	public void setEnemies(int i, int j, int k, int l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
