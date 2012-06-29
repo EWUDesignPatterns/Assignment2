256647e3e6092c187891f678db5a29c16d739c11
Jon Roster <jroster76@gmail.com>
2012-06-28 19:14:30 -0700

    -initial commit of tester file

---
+++ src/TestSauronEye.java
@@ -0,0 +1,17 @@
+public class TestSauronEye
+{
+    public static void main(String[] args) 
+    {
+
+        EyeOfSauron eye = new EyeOfSauron();
+        BadGuy saruman = new BadGuy(eye, "Saruman");
+        BadGuy angmar = new BadGuy(eye, "Angmar");
+        eye.setEnemies(1, 1, 2, 0); //hobbits, elves, dwarves, men
+        //message should be displayed from Saruman and Angmar that they know about 1 hobbit, 1 elf, 2 dwarves
+        
+        saruman.defeated(); //Saruman is no longer registered with the Eye
+        eye.setEnemies(4, 2, 2, 100);
+        //only Angmar reports on the enemies
+
+    }//end main
+}//end class
